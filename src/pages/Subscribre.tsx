bb17da7f1fca734dc67ee240fa445b2c1260dd87
jonesPC <jonescnoel@gmail.com>
2022-06-24 21:41:53 -0300

    responsive Subscribre

--- src/pages/Subscribre.tsx
+++ src/pages/Subscribre.tsx
@@ -2,7 +2,6 @@ import { useState, FormEvent } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Logo } from '../components/Logo'
 import { useCreateSubscriberMutation } from '../graphql/generated'
-import CodeMackup from '../assets/code-mackup.png'
 
 const codeMackup = new URL('../assets/code-mackup.png', import.meta.url).href
 
@@ -29,19 +28,19 @@ export const Subscribre = () => {
   }
 
   return (
-    <div className='min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center'>
-      <div className='w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto'>
-        <div className='max-w-[640px]'>
+    <div className='min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center p-4'>
+      <div className='w-full max-w-[1100px] flex flex-col lg:flex-row items-center justify-between mt-10 lg:mt-20 mx-auto'>
+        <div className='max-w-[640px] flex flex-col items-center lg:block'>
           <Logo />
-          <h1 className='mt-8 text-[2.5rem] leading-tight'>
+          <h1 className='mt-8 text-[1.25rem] lg:text-[2.5rem] leading-tight'>
             Construa uma <strong className='text-blue-500'>aplicação completa</strong>, do zero, com <strong className='text-blue-500'>React</strong>
           </h1>
-          <p className='mt-4 text-gray-200 leading-relaxed'>
+          <p className='mt-4 text-gray-200 leading-relaxed text-center lg:text-left'>
             Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas e com alta demanda para acessar as melhores oportunidades do mercado.
           </p>
         </div>
 
-        <div className='p-8 bg-gray-700 border border-gray-500 rounded'>
+        <div className='p-8 bg-gray-700 border border-gray-500 rounded mt-[32px] lg:mt-0'>
           <strong className='text-2xl mg-6 block'>Inscreva-se gratuitamente</strong>
 
           <form onSubmit={handleSubscribe} className='flex flex-col gap-2 w-full'>
@@ -67,7 +66,7 @@ export const Subscribre = () => {
           </form>
         </div>
       </div>
-      <img src={codeMackup} className='mt-10' alt="" />
+      <img src={codeMackup} className='mt-4 lg:mt-10' alt="" />
     </div>
   )
 }
