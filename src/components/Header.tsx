f2a54a9f67772e3e35fd5a4917fd931aefccc8d7
jonesPC <jonescnoel@gmail.com>
2022-06-25 07:34:24 -0300

    Responsive > menu dropdown Adicionado botao hamburger mobile/tablet Compartilhamento de State ( isNavOpen, screen ) nos componentes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Logo } from "./Logo";
+import classNames from 'classnames'
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export const Header = () => {
+interface HeaderProps {
+  isNavOpen: boolean,
+  setIsNavOpen: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+export const Header = ({ isNavOpen, setIsNavOpen }: HeaderProps) => {
+
   return (
-    <header className="w-full py-5 flex items-center justify-center bg-gray-700 border-b border-gray-600">
-      <Logo />
+    <header className={classNames(" w-full py-5 px-6 flex items-center justify-between lg:justify-center bg-gray-700 border-b border-gray-600", {
+
+    })}>
+      <Logo width="167" />
+      <nav className="flex items-center gap-3 ">
+        <span className="text-sm">Aulas</span>
+        <button className={!isNavOpen ? 'flex' : 'hidden'}>
+          <FontAwesomeIcon icon={faBars}
+            className="text-2xl animate-pulse text-blue-500"
+            onClick={() => setIsNavOpen((prev) => !prev)}
+          />
+        </button>
+        <button className={isNavOpen ? 'flex' : 'hidden'}>
+          <FontAwesomeIcon icon={faXmark}
+            className="text-2xl text-blue-500"
+            onClick={() => setIsNavOpen(false)}
+          />
+        </button>
+
+      </nav>
     </header>
   );
 };
--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
@@ -7,16 +8,29 @@ import { Video } from "../components/Video";
 export const Event = () => {
   const { slug } = useParams<{ slug: string }>()
 
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  const [screen, setScreen] = useState<number>(0)
+
+  useEffect(() => {
+    updateDimensions();
+    window.addEventListener("resize", updateDimensions);
+  }, []);
+
+  const updateDimensions = () => {
+    const width = window.innerWidth;
+    setScreen(width)
+  };
+
   return (
     <div className="flex flex-col min-h-screen">
-      <Header />
+      <Header isNavOpen={isNavOpen} setIsNavOpen={setIsNavOpen} />
       <main className="flex flex-1">
         {slug
           ? <Video lessonSlug={slug} />
           : <div className="flex-1" />
         }
+        {screen > 1023 && <Sidebar />}
 
-        <Sidebar />
       </main>
     </div>
   );
